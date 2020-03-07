b02ac17da5edb6ad98d5e2d16c720c1fa135d89a
ogihara-ryo <ogihara.ryo.b@gmail.com>
2020-03-08 03:12:03 +0900

    Add types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
-const Home = ({ userAgent }) => <h1>Hello world! - user agent: {userAgent}</h1>;
+import { NextPage } from 'next';
+
+const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
+  <h1>Hello world! - user agent: {userAgent}</h1>
+);
 
 Home.getInitialProps = async ({ req }) => {
-  const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
+  const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
   return { userAgent }
 };
 
